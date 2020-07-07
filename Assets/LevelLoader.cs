d14ad9752a27e04b2d765a2933d2b45ea836a111
bobojo <williamsokol0@gmail.com>
2020-07-06 22:34:28 -0700

    added win/lose screen

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Fade fade;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        
+        print(fade);
+    }
+
+    public void LoadNextLevel(){
+        fade = GameObject.Find("Fade").GetComponent<Fade>();
+        fade.FadeOut();
+        Invoke("LoadingLevel", 1f);
+        
+    }
+    void LoadingLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Lose()
+    {
+        fade = GameObject.Find("Fade").GetComponent<Fade>();
+        print(fade);
+        fade.FadeOut();
+        Invoke("LoseLevel",1);
+    }
+    void LoseLevel()
+    {
+        SceneManager.LoadScene("LossScene");
+    }
+}
---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+   public Animator animator;
+   public float animLength;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        //animLength = 
+        
+    }
+
+  public void FadeOut()
+  {
+      animator.SetTrigger("LoadingScene");
+  }
+}
