dd78d326d7251003d6a7cb9b2ac5e092b82f42e9
bobojo <williamsokol0@gmail.com>
2020-07-08 15:40:17 -0700

    little fix

---
+++ Assets/Scripts/Music/MenuSounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSounds : MonoBehaviour
+{
+    //int count =0;
+    private AudioSource soundPlayer;
+
+    public AudioClip loadLevelNoise;
+    //public AudioClip playerJumpNoise;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        soundPlayer = GetComponent<AudioSource>();
+
+    }
+
+
+    public void MenuNoise()
+    {
+        soundPlayer.clip = loadLevelNoise;
+        soundPlayer.Play();
+    }
+    
+    
+}
