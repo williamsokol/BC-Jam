6148872a0a18403d302841a34c8d11696ea5e747
ChasteCrash <chastecrash_10@hotmail.com>
2020-07-08 19:46:35 -0500

    Coin system

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coin : MonoBehaviour
+{
+    public int coinValue = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                ScoreMannager.instance.ChangeScore(coinValue);
+                Destroy(this.gameObject);
+            }
+
+        }
+    }
+
+
+}
